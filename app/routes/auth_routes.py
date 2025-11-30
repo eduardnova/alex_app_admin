6f3a686f747b7ff94fac6f3bc44397b2ff081c53
eduardnova <rubeneduardonova@gmail.com>
2025-11-30 12:03:37 -0400

    update 2

--- app/routes/auth_routes.py
+++ app/routes/auth_routes.py
@@ -1,5 +1,5 @@
 """
-Authentication Routes - Login, Logout, Password Reset
+Authentication Routes - Login, Logout, Password Reset .
 """
 from flask import Blueprint, render_template, redirect, url_for, flash, request
 from flask_login import login_user, logout_user, login_required, current_user
